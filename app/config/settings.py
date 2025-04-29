efb1c0fbecd39700cdfceb4ef8caa88ae910992b
Ayat Aboalnaga <ayat.aboalnaga@gmail.com>
2025-04-29 08:48:56 +0300

    config: update application settings to load environment variables

--- app/config/settings.py
+++ app/config/settings.py
@@ -4,6 +4,9 @@ from pydantic import RedisDsn
 class Settings(BaseSettings):
     REDIS_URL: RedisDsn = "redis://localhost:6379"
     RAGAS_THRESHOLD: float = 0.7
-    API_KEY: str = "your_super_secret_key" # Replace with a secure method for storing and retrieving keys
+    API_KEY: str
+
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
